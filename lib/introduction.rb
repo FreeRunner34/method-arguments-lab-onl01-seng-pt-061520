82c29d92c60b324fcfcf6eb8994bc685415bd46f
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-16 04:34:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,6 @@ end
 
 introduction("Nikolaus")
 
-language = "Ruby"
 def introduction_with_language(name, language)
   puts "hi, my name is #{name} and i am learning to program in #{language}."
 end
