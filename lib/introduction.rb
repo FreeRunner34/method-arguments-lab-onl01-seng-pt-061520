e6b002f51e1fd3924dbe7ffd4133d9e4b71c4e2d
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-16 04:31:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,10 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-introduction(Nikolaus)
+introduction("Nikolaus")
 
 def introduction_with_language(name, language)
   puts "hi, my name is #{name} and i am learning to program in #{language}."
 end
 
-introduction_with_language(Nikolaus, Ruby)
+introduction_with_language("Nikolaus", "Ruby")
